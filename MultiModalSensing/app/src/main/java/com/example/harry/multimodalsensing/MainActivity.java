61750afe3292fc84b74031fc25c814335ba807f0
hkiang01 <hkiang01@yahoo.com>
2016-02-10 21:13:16 -0600

    stage 1, untested

---
+++ MultiModalSensing/app/src/main/java/com/example/harry/multimodalsensing/MainActivity.java
@@ -0,0 +1,99 @@
+package com.example.harry.multimodalsensing;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+    private SensorManager sensorManager;
+    TextView AccelXValueView;
+    TextView AccelYValueView;
+    TextView AccelZValueView;
+
+    TextView GyroXValueView;
+    TextView GyroYValueView;
+    TextView GyroZValueView;
+
+    TextView MagnetXValueView;
+    TextView MagnetYValueView;
+    TextView MagnetZValueView;
+
+    TextView LightValueView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_scrolling);
+
+        AccelXValueView=(TextView)findViewById(R.id.AccelXcoordView);
+
+        sensorManager=(SensorManager)getSystemService(SENSOR_SERVICE);
+
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE),
+                SensorManager.SENSOR_DELAY_NORMAL);
+
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),
+                SensorManager.SENSOR_DELAY_NORMAL);
+
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT),
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(event.sensor.getType()==Sensor.TYPE_ACCELEROMETER){
+            // assign directions
+            float x=event.values[0];
+            float y=event.values[1];
+            float z=event.values[2];
+
+            AccelXValueView.setText("X: "+x);
+            AccelYValueView.setText("Y: "+y);
+            AccelZValueView.setText("Z: "+z);
+        }
+
+        else if(event.sensor.getType()==Sensor.TYPE_GYROSCOPE) {
+            float x=event.values[0];
+            float y=event.values[1];
+            float z=event.values[2];
+
+            GyroXValueView.setText("X: "+x);
+            GyroYValueView.setText("Y: "+y);
+            GyroZValueView.setText("Z: "+z);
+        }
+
+        else if(event.sensor.getType()==Sensor.TYPE_MAGNETIC_FIELD) {
+            float x=event.values[0];
+            float y=event.values[1];
+            float z=event.values[2];
+
+            MagnetXValueView.setText("X: "+x);
+            MagnetYValueView.setText("Y: "+y);
+            MagnetZValueView.setText("Z: "+z);
+        }
+
+        else if(event.sensor.getType()==Sensor.TYPE_LIGHT) {
+            float l=event.values[0];
+
+            LightValueView.setText("Light: "+l);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
