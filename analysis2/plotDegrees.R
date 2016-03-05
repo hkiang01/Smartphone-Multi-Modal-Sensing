8c3cd47751d09d1848bf5ebc4e6a8e91d2f43596
hkiang01 <hkiang01@yahoo.com>
2016-03-05 03:20:08 -0600

    Direction and color in plot

--- analysis2/plotDegrees.R
+++ analysis2/plotDegrees.R
@@ -1,15 +1,16 @@
 library('stringr')
 
 normalized_time_interval<-200 # in ms
-col_of_interest<-14 # the col storing the variable of interest
-
+degree_col<-14
+dir_col<-16
 setwd('/Users/harry/projects/ece498rr/analysis2/')
 wdat<-read.csv('ACTIVITY_05-03-16_00-51-34-051.csv', header=FALSE, na.strings=c("", "null", "NA"))
-date_and_degrees<-wdat[,c(1,col_of_interest)]
+
+date_and_degrees<-wdat[,c(1,degree_col, dir_col)]
 date_and_degrees<-na.omit(date_and_degrees) #remove na's
 date_and_degrees<-t(date_and_degrees)
 date_and_degrees<-na.omit(date_and_degrees) #remove na's
-new_dnd<-matrix(ncol=2)
+new_dnd<-matrix(ncol=3)
 
 
 #normalize to 200ms timestamps
@@ -18,23 +19,40 @@ zeroTS_string<-substr(date_and_degrees[,c(1)][1],tsLen-11,tsLen) #extract ms for
 zeroTS_string<-str_replace_all(zeroTS_string, ":", "")
 zeroTS<-as.numeric(zeroTS_string)
 nextTS<-zeroTS+normalized_time_interval
-new_dnd<-rbind(new_dnd, c(0, date_and_degrees[,c(1)][2]))
+new_dnd<-rbind(new_dnd, c(0, date_and_degrees[,c(1)][2], date_and_degrees[,c(1)][3]))
 count<-1
 for(i in 2:NCOL(date_and_degrees)) {
  curTS_string<-substr(date_and_degrees[,c(i)][1],tsLen-11,tsLen) #extract ms for "zero" time
  curTS_string<-str_replace_all(curTS_string, ":", "")
  curTS<-as.numeric(curTS_string)
  if(!is.na(curTS >= nextTS) && curTS >= nextTS) {
-   print(nextTS)
-   print(curTS)
-   new_dnd<-rbind(new_dnd, c(normalized_time_interval * count, date_and_degrees[,c(i)][2]))
+   new_dnd<-rbind(new_dnd, c(normalized_time_interval * count, date_and_degrees[,c(i)][2], date_and_degrees[,c(i)][3]))
    nextTS<-nextTS+normalized_time_interval
    count<-count+1
  }
 }
 
 new_dnd<-na.omit(new_dnd)
-
 #at this point, new_dnd has degree readings at normalized time intervals of normalized_time_interval
-
-plot(new_dnd[,c(1)], new_dnd[,c(2)], type="l", main="Bearing Degrees Over Time", xlab="Time in ms", ylab="Bearing Degrees")
+colMatrix<-matrix()
+t_new_dnd<-t(new_dnd)
+for(i in 1:NCOL(t_new_dnd)) {
+  curDir<-t_new_dnd[,c(i)][3]
+  if(curDir=="N") {
+    colMatrix<-rbind(colMatrix, "black")
+  }
+  else if(curDir=="E") {
+    colMatrix<-rbind(colMatrix, "red")
+  }
+  else if(curDir=="S") {
+    colMatrix<-rbind(colMatrix, "green")
+  }
+  else if(curDir=="W") {
+    colMatrix<-rbind(colMatrix, "blue")
+  }
+}
+colMatrix<-na.omit(colMatrix)
+colMatrix<-t(colMatrix)
+par(xpd=TRUE)
+plot(new_dnd[,c(1)], new_dnd[,c(2)], col=colMatrix, type="h", main="Bearing Degrees Over Time", xlab="Time in ms", ylab="Bearing Degrees")
+legend(0,-60,c("N", "E", "S", "W"),col=c("black", "red", "green", "blue"),pch=1, horiz=TRUE)
